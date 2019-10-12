8d92c1f868e9cc562000ef8b22f4b38ff0206a8d
bprinty <bprinty@gmail.com>
2019-10-12 10:27:46 -0500

    Changed description for package

--- flask_occam/__init__.py
+++ flask_occam/__init__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
-__pkg__ = 'Flask-Plugin'
-__url__ = 'https://github.com/bprinty/scaffolds/flask-plugin'
-__info__ = 'Scaffold for example Flask extension'
+__pkg__ = 'Flask-Occam'
+__url__ = 'https://github.com/bprinty/Flask-Occam'
+__info__ = 'Flask extension for simplifying REST API development.'
 __author__ = 'Blake Printy'
 __email__ = 'bprinty@gmail.com'
 __version__ = '0.1.0'
 
 
 from .mixins import ExampleMixin      ## noqa
-from .plugin import Occam            ## noqa
+from .plugin import Occam             ## noqa
