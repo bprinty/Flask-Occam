4ac557381c88f2dbbabeae35467c093e128f03f2
bprinty <bprinty@gmail.com>
2019-10-16 23:40:05 -0500

    Added database overrides

--- flask_occam/plugin.py
+++ flask_occam/plugin.py
@@ -7,7 +7,10 @@
 
 # imports
 # -------
+from flask import Flask
+
 from .converters import ModelConverter
+from .mixins import ModelMixin
 
 
 # plugin
@@ -18,7 +21,20 @@ class Occam(object):
     routing.
     """
 
-    def __init__(self, app=None):
+    def __init__(self, app=None, db=None):
+        
+        # arg mismatch
+        if app is not None and \
+           db is None and \
+           not isinstance(app, Flask):
+            self.init_db(app)
+            app = None
+
+        # proper spec
+        if db is not None:
+            self.init_db(db)
+        
+        # app is specified properly
         if app is not None:
             self.init_app(app)
         return
@@ -27,4 +43,9 @@ class Occam(object):
         self.app = app
         self.app.config.setdefault('OCCAM_LOG_USER_FORMAT', 'user')
         self.app.url_map.converters['id'] = ModelConverter
+        return
+
+    def init_db(self, db):
+        self.db = db
+        self.db.Model.__bases__ += (ModelMixin,)
         return
