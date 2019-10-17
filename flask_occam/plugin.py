2bd5f62d2332624280eb4bf5b6d76c315d3e9082
bprinty <bprinty@gmail.com>
2019-10-17 00:22:37 -0500

    Added @route extension to plugin, enabling class-based api definitions

--- flask_occam/plugin.py
+++ flask_occam/plugin.py
@@ -7,11 +7,90 @@
 
 # imports
 # -------
-from flask import Flask
+from flask import Flask, Blueprint
+import types
 
 from .converters import ModelConverter
 from .mixins import ModelMixin
 from .errors import ValidationError
+
+
+# config
+# ------
+METHODS = [
+    'GET',
+    'HEAD',
+    'POST',
+    'PUT',
+    'DELETE',
+    'CONNECT',
+    'OPTIONS',
+    'TRACE',
+    'PATCH'
+]
+
+
+# helpers
+# -------
+def route(self, rule, **options):
+    """
+    Decorator for registering view function for a given URL rule.
+    This ovverrides the default Flask route decorator to be able
+    to handle class-based definitions, where endpoints are specified as
+    class methods. With this, you can define routes with a function
+    like you normally do in Flask:
+
+    .. code-block:: python
+
+        @app.route('/')
+        def index():
+            return 'Hello World'
+
+
+    Or, you can define them with a class:
+
+    .. code-block:: python
+
+        @app.route('/test')
+        class Test:
+            def get(self):
+                return 'GET /test', 200
+            def post(self):
+                return 'POST /test', 201
+
+
+    This class-based definition of handlers helps with code organization
+    and documentation efforts.
+
+    """
+    def decorator(obj):
+
+        # function-based definition
+        endpoint = options.pop("endpoint", obj.__name__)
+        app = self.app if hasattr(self, 'app') else self
+        if isinstance(obj, types.FunctionType):
+            app.add_url_rule(rule, endpoint, obj, **options)
+
+        # class-based definition
+        else:
+            instance = obj()
+            methods = options.pop("methods", METHODS)
+            for meth in methods:
+                handler = meth.lower()
+                if hasattr(instance, handler):
+                    endpoint = handler + '_' + obj.__name__.lower()
+                    options['methods'] = [meth]
+                    func = getattr(instance, handler)
+                    app.add_url_rule(
+                        rule, endpoint, func,
+                        **options
+                    )
+
+        return obj
+    return decorator
+
+
+Blueprint.route = types.MethodType(route, Blueprint)
 
 
 # plugin
@@ -23,6 +102,7 @@ class Occam(object):
     """
 
     def __init__(self, app=None, db=None):
+        self.Blueprint = Blueprint
         
         # arg mismatch
         if app is not None and \
@@ -42,6 +122,7 @@ class Occam(object):
 
     def init_app(self, app):
         self.app = app
+        self.app.route = types.MethodType(route, self.app)
         self.app.config.setdefault('OCCAM_LOG_USER_FORMAT', 'user')
         self.app.url_map.converters['id'] = ModelConverter
         self.app.register_error_handler(ValidationError, ValidationError.handler)
