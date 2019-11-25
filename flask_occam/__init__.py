a9035d5db1988f1a0056c2797bfc36ba134305d2
bprinty <bprinty@gmail.com>
2019-11-25 06:27:30 -0600

    Incremented patch version for bug fix

--- flask_occam/__init__.py
+++ flask_occam/__init__.py
@@ -5,7 +5,7 @@ __url__ = 'https://github.com/bprinty/Flask-Occam'
 __info__ = 'Flask extension for simplifying REST API development.'
 __author__ = 'Blake Printy'
 __email__ = 'bprinty@gmail.com'
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 
 
 from .plugin import Occam, Blueprint         ## noqa
